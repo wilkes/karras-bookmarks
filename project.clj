d6a2aaa83611f0d1ce023e7ea9b806ef0a2df41c
Seth Buntin <seth.buntin@gmail.com>
2010-07-16 10:29:51 -0500

    update dep versions; add robert-hook

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "An example karras web service"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [clojure-contrib "1.2.0-SNAPSHOT"]
-                 [compojure "0.4.0-RC3"]
+                 [compojure "0.4.1"]
                  [karras "0.4.0-SNAPSHOT"]
-                 [ring/ring-jetty-adapter "0.2.0"]]
+                 [robert/hooke "1.0.2"]
+                 [ring/ring-jetty-adapter "0.2.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [bookmarks.core])
