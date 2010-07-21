b2e3a4d141c34e14d326da901d1e848113844889
Seth Buntin <seth.buntin@gmail.com>
2010-07-21 09:00:25 -0500

    update to karras 0.4.0; move example to mustache framework

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "An example karras web service"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [clojure-contrib "1.2.0-SNAPSHOT"]
-                 [compojure "0.4.1"]
-                 [karras "0.4.0-SNAPSHOT"]
+                 [karras "0.4.0"]
+                 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
                  [robert/hooke "1.0.2"]
                  [ring/ring-jetty-adapter "0.2.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
--- src/bookmarks/core.clj
+++ src/bookmarks/core.clj
@@ -2,13 +2,16 @@
   (:require [karras.core :as k]
             [karras.collection :as c])
   (:use [clojure.contrib.json :only [json-str Write-JSON]]
-        [compojure.core :only [defroutes GET POST PUT DELETE routes]]
+        [net.cgrand.moustache :only [app]]
         [ring.adapter.jetty :only [run-jetty]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
+        [ring.middleware.params :only [wrap-params]]
         karras.entity
-        karras.sugar))
+        karras.sugar
+        clojure.pprint)
+  (:import [org.bson.types ObjectId]))
 
-(extend org.bson.types.ObjectId Write-JSON
+(extend ObjectId Write-JSON
         {:write-json (fn [x out] (.print out (str "\"" x "\"")))})
 
 (defentity User
@@ -53,66 +56,65 @@
        {:status 500 :body (json-str {:type (class e#)
                                      :message (.getMessage e#)})})))
 
-(defn read-objectid [request]
-  (org.bson.types.ObjectId. (get (:params request) "id")))
-
 (defn list-resources 
   "GET /{collection-name}"
-  [request type]
-  (json-respond (fetch-all type)))
+  [type]
+  (fn [request]
+    (json-respond (fetch-all type))))
 
 (defn create-resource 
   "POST /{collection-name}"
-  [request type]
-  (json-respond (create type (read-params-for type (:params request)))))
+  [type]
+  (fn [request]
+    (json-respond (create type (read-params-for type (:params request))))))
 
 (defn get-resource
   "GET /{collection-name}/:id"
-  [request type]
-  (json-respond (fetch-one type
-                           (where (eq :_id (read-objectid request))))))
+  [type id]
+  (fn [request]
+    (json-respond (fetch-one type
+                             (where (eq :_id (ObjectId. id)))))))
 
 (defn update-resource
   "PUT /{collection-name}/:id"
-  [request type]
-  (json-respond (update type
-                        (where (eq :_id (read-objectid request)))
-                        (modify (set-fields (read-params-for type (:params request)))))))
+  [type id]
+  (fn [request]
+    (json-respond (find-and-modify type
+                                   (where (eq :_id (ObjectId. id)))
+                                   (modify (set-fields (read-params-for
+                                                        type (:params request))))
+                                   :return-new true))))
 
 (defn delete-resource
   "DELETE /{collection-name}/:id"
-  [request type]
-  (json-respond (delete type
-                        (where (eq :_id (read-objectid request))))))
+  [type id]
+  (fn [request]
+    (json-respond (find-and-remove type
+                                   (where (eq :_id (ObjectId. id)))))))
 
-(defmacro defresource [type & options]
-  (let [name (:collection-name (entity-spec (resolve type)))
-        nsym (symbol (str name "-routes"))]
-    `(defroutes ~nsym
-       (GET (str "/" ~name) req# (list-resources req# ~type))
-       (POST (str "/" ~name) req# (create-resource req# ~type))
-       (GET (str "/" ~name "/:id") req# (get-resource req# ~type))
-       (PUT (str "/" ~name "/:id") req# (update-resource req# ~type))
-       (DELETE (str "/" ~name "/:id") req# (delete-resource req# ~type)))))
+(defn resource-app [type & options]
+  (app [] {:get (list-resources type)
+           :post (create-resource type)}
+       [id] {:get (get-resource type id)
+             :put (update-resource type id)
+             :delete (delete-resource type id)}))
 
 (defonce bookmark-db (k/mongo-db :karras-bookmark))
 
-(defn wrap-mongo-request [app]
+(defn wrap-mongo-request [handler]
   (fn [request]
     (k/with-mongo-request bookmark-db
-      (app request))))
+      (handler request))))
 
-;; These generate routes named 'bookmarks-routes' and 'users-routes'
-(defresource Bookmark)
-(defresource User)
-
-(def app
-     (-> (routes bookmarks-routes users-routes)
+(def application
+     (-> (app ["bookmarks" &] (resource-app Bookmark)
+              ["users" &] (resource-app User))
+         wrap-params
          wrap-keyword-params
          wrap-mongo-request))
 
 (defn -main [& args]
   (let [[port] args]
-    (run-jetty (var app)
+    (run-jetty (var application)
                {:port (Integer. (or port 8080))
                 :join? false})))
